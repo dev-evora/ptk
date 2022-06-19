9feb51913937d1c7ecc077d26dec24ff620ec68d
Slava Khripushin <slava.cpa@yandex.ru>
2022-06-19 14:13:20 +0300

    decor

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,49 @@ const maskPhone = () => {
 };
 
 maskPhone();
+
+$('.decor-list li').each(function (i, item) {
+  $(item).attr('data-id', i);
+});
+
+$('.decor-content').each(function (i, item) {
+  $(item).attr('data-id', i);
+});
+
+$('.decor-list li[data-id=0]').addClass('active');
+$('.decor-content[data-id=0]').addClass('active');
+
+$('.decor-list li').click(function () {
+  const id = $(this).attr('data-id');
+
+  $('.decor-list li').removeClass('active');
+  $('.decor-list li[data-id=' + id + ']').addClass('active');
+
+  $('.decor-content').removeClass('active');
+  $('.decor-content[data-id=' + id + ']').addClass('active');
+});
+
+if ($('.decor-slider').length) {
+  $('.decor-slider').each(function (i, item) {
+    $('.decor-slider__thumbs', item).attr('id', 'thumbs-' + i);
+    $('.decor-slider__main', item).attr('id', 'main-' + i);
+  });
+
+  $('.decor-slider').each(function (i, item) {
+    const thumbs = new Swiper('.decor-slider__thumbs#thumbs-' + i, {
+      observer: true,
+      observeParents: true,
+      watchSlidesProgress: true,
+      spaceBetween: 20,
+      slidesPerView: 7,
+      direction: 'vertical',
+    });
+
+    const main = new Swiper('.decor-slider__main#main-' + i, {
+      watchSlidesProgress: true,
+      thumbs: {
+        swiper: thumbs,
+      },
+    });
+  });
+}
